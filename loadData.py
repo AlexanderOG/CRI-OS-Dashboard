86101be654c40a7486c132e32680c8d81c14e272
AlexanderOG <alexander_obeid@hotmail.com>
2022-04-01 14:15:21 +0200

    Add basic code to load data

---
+++ loadData.py
@@ -0,0 +1,8 @@
+import pandas as pd
+confirmed_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+
+print("Shape of confirmed cases:", confirmed_cases.shape)
+print("Shape of deaths:", deaths.shape)
+print("Shape of recovered:", recovered.shape)
