73dddc5be4e3b183bc9a8b5a25a93672d38781c4
Azadi97 <gworld97@gmail.com>
2022-04-08 11:55:44 +0200

    updated data source url

--- loadData.py
+++ loadData.py
@@ -2,7 +2,9 @@ import pandas as pd
 confirmed_cases = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 deaths = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 recovered = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+owid_covid_data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 
 print("Shape of confirmed cases:", confirmed_cases.shape)
 print("Shape of deaths:", deaths.shape)
 print("Shape of recovered:", recovered.shape)
+print("Shape of recovered:", owid_covid_data)
