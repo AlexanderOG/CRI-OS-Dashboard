4878bf903395f8daf0c33e13f84a876bced3e7d4
AlexanderOG <alexander_obeid@hotmail.com>
2022-04-08 15:55:26 +0200

    Add data type selection

--- cov_dashboard.py
+++ cov_dashboard.py
@@ -14,14 +14,16 @@ df = loadData()
 
 # Get the countries list
 clist = df['location'].unique()
+typeList = {'New cases':'new_cases', 'New cases smoothed (7-day rolling average)':'new_cases_smoothed', 'Total cases': 'total_cases', 'New deaths':'new_deaths', 'New deaths smoothed (7-day rolling average)':'new_deaths_smoothed', 'Total deaths': 'total_deaths'}
 
 # Create the streamlit sidebar
 country = st.sidebar.selectbox("Select a country:",clist)
-
+dataType = st.sidebar.selectbox("Select the type of data you want to see:",typeList.keys())
+dataTypeSelection = typeList[dataType]
 # The header of the figure
 st.header("COVID-19 cases per country")
 
-cases = df[df['location'] == country]['new_cases']
+cases = df[df['location'] == country][dataTypeSelection]
 dates = list(df[df['location'] == country]['date'])
 
 # Create a figure
@@ -29,6 +31,7 @@ fig, ax = plt.subplots()
 # plot data
 ax.plot( dates, cases)
 ax.tick_params(axis='x',rotation=90)
+st.write(dataType + " for " + country)
 
 
 # Plots the chart
