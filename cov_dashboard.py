ea1afca62e50db7bdf327528b02828c1213641f3
Azadi97 <gworld97@gmail.com>
2022-04-19 16:03:14 +0200

    multiSelector added

--- cov_dashboard.py
+++ cov_dashboard.py
@@ -37,61 +37,42 @@ normalized = st.sidebar.checkbox('Normalized data (per million habitants of the 
 if normalized:
     dataType += " per million habitants of the country"
     dataTypeSelection += "_per_million"
+    
 # The header of the figure
-st.header("COVID-19 cases per country")
+# st.header("COVID-19 cases per country")
+st.markdown("<h2 style='text-align: left; color: #ff4b4b;'>COVID-19 cases per country</h2>", unsafe_allow_html=True)
 
-<<<<<<< HEAD
-cases = df[df['location'] == country]['new_cases']
-dates = list(df[df['location'] == country]['date'])
-
-# Create a figure
-fig, ax = plt.subplots()
-# plot data
-ax.plot( dates, cases)
-ax.tick_params(axis='x',rotation=90)
-
-
-# Plots the chart
-st.plotly_chart(fig)
-
-# ---------------------------------------------------------
-# # MultySelector goes here
-# # Get the countries list
-clist = df['location'].unique()
-
-# Create the streamlit sidebar
-# country = st.sidebar.selectbox("Select a country:",clist)
-country = st.sidebar.multiselect("Select countries:",clist,'Afghanistan')
-# The header of the figure
-st.header("COVID-19 cases per country - Multy Countries")
-
-cases = df[df['location'] == country]['new_cases']
-=======
 cases = df[df['location'] == country][dataTypeSelection]
->>>>>>> b7375690e514894a15c5c77e01ef3148b01d8dc6
 dates = list(df[df['location'] == country]['date'])
 
+#---------------------------------
+####-- OLD CODES --####
+
 # Create a figure
-fig, ax = plt.subplots()
-# plot data
-ax.plot( dates, cases)
-ax.tick_params(axis='x',rotation=90)
-ax.set_xlabel("Date")
-ax.set_ylabel(dataType)
-st.write(country)
+# fig, ax = plt.subplots()
+# # plot data
+# ax.plot( dates, cases)
+# ax.tick_params(axis='x',rotation=90)
+# ax.set_xlabel("Date")
+# ax.set_ylabel(dataType)
+# st.write(country)
 
 
+####-- NEW CODES REPLACED --####
+
+fig = px.line(df[df['location'] == country], x=dates, y=cases, color="location", width=850, height=450)
+
+#-----------------------------------
 # Plots the chart
 st.plotly_chart(fig)
-# ---------------------------------------------------------
-# data = df
 
-# # Create a list of possible values and multiselect menu with them in it.
-# COUNTRIES = df['location'].unique()
-# COUNTRIES_SELECTED = st.sidebar.multiselect('Select countries', COUNTRIES)
 
-# # Mask to filter dataframe
-# mask_countries = df['COUNTRIES_SELECTED'].isin(COUNTRIES)
+# MULTI COUNTRY SELECTOR
 
-# # df = df[mask_countries]
-# st.write('You selected:', mask_countries)
+# st.header("COVID-19 new cases (Multi Country Selector)")
+st.markdown("<h2 style='text-align: left; color: #ff4b4b;'>COVID-19 new cases (Multi Country Selector)</h2>", unsafe_allow_html=True)
+select1 = st.sidebar.multiselect('Select Countries', clist, default=["Afghanistan"], key='2')
+
+# if len(select1) > 0:
+fig = px.line(df[df['location'].isin(select1)], x="date", y="new_cases", color="location", width=850, height=450)
+st.plotly_chart(fig)
